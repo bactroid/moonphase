787616bd68561d7ada50747fd9279e30422e1cac
Kate Haskell <fuzzcat@bactroid.net>
2016-09-04 14:50:42 -0400

    Next moon phases will now display in correct order, output moved into appropriate functions

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 var lune = require('lune');
 
 // Get date range for phase_range
-var today = new Date();
-var endDate = getEndDate(today);
+var startDate = new Date();
+var endDate = getEndDate(startDate);
 
 // Load next full moon date
-phases = lune.phase_range(today, endDate, lune.PHASE_FULL);
+phases = lune.phase_range(startDate, endDate, lune.PHASE_FULL);
 var fullMoon = new Date(phases[0]);
 
 // Load next new moon date
-phases = lune.phase_range(today, endDate, lune.PHASE_NEW);
+phases = lune.phase_range(startDate, endDate, lune.PHASE_NEW);
 var newMoon = new Date(phases[0]);
 
-console.log('Next full moon is on: \n' + fullMoon);
-console.log('Next new moon is on: \n' + newMoon);
+if (newMoon > fullMoon) {
+    printNextFullMoon(fullMoon);
+    printNextNewMoon(newMoon);
+}
+
+else {
+    printNextNewMoon(newMoon);
+    printNextFullMoon(fullMoon);
+}
 
 // Returns the day 30 days from startDate
 function getEndDate (startDate) {
     var date = new Date(startDate.toDateString());
     date.setDate(date.getDate() + 30);
     return date;
+}
+
+// Log next new moon date to the console
+function printNextNewMoon(date) {
+    console.log('Next new moon is on: \n' + date);
+}
+
+// Log next full moon date to the console
+function printNextFullMoon(date) {
+    console.log('Next full moon is on: \n' + date);
 }
