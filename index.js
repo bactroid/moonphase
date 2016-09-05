e490be0165b42c21d49132b59c93c2a48453536e
Kate Haskell <fuzzcat@bactroid.net>
2016-09-05 08:27:19 -0400

    Moon phase for date is now displayed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getPhases(d) {
     }
 
     else {
-        if (validator.isDate(d)) var startDate = new Date(d);
+        if (validator.isDate(d)) var startDate = new Date(d + ' 05:00');
         else {
             console.error('Invalid date format');
             return;
@@ -24,6 +24,8 @@ function getPhases(d) {
     // Load next new moon date
     phases = lune.phase_range(startDate, endDate, lune.PHASE_NEW);
     var newMoon = new Date(phases[0]);
+
+    printMoonPhase(startDate);
 
     // Logic to display appropriate next moon phases
     // We don't want to describe the phases out of order.
@@ -43,6 +45,49 @@ function getEndDate (startDate) {
     var date = new Date(startDate.toDateString());
     date.setDate(date.getDate() + 30);
     return date;
+}
+
+// Log the current phase of the moon with a natural language response
+// and a percentage
+// e.g. "Waxing Crescent (25%)"
+function printMoonPhase(checkDate) {
+    phaseList = lune.phase_hunt(checkDate);
+
+    // Populate 'day only' dates of the phases and user date
+    var newMoon = new Date(phaseList.new_date);
+    var firstQuarter = new Date(phaseList.q1_date);
+    var fullMoon = new Date(phaseList.full_date);
+    var lastQuarter = new Date(phaseList.q3_date);
+
+    newMoon.setHours(0, 0, 0, 0);
+    firstQuarter.setHours(0, 0, 0, 0);
+    fullMoon.setHours(0, 0, 0, 0);
+    lastQuarter.setHours(0, 0, 0, 0);
+    checkDate.setHours(0, 0, 0, 0);
+
+    // If date's phase matches a named phase...
+    // Oddly enough, these comparisons won't work without valueOf().
+    if (checkDate.valueOf() === newMoon.valueOf())
+        var phaseText = 'New Moon';
+    else if (checkDate.valueOf() === firstQuarter.valueOf())
+        var phaseText = 'First Quarter';
+    else if (checkDate.valueOf() === fullMoon.valueOf())
+        var phaseText = 'Full Moon';
+    else if (checkDate.valueOf() === lastQuarter.valueOf())
+        var phaseText = 'Last Quarter';
+
+    // If date's phase doesn't match one of the big four...
+    else if (checkDate > newMoon && checkDate < firstQuarter)
+        var phaseText = 'Waxing Crescent';
+    else if (checkDate > firstQuarter && checkDate < fullMoon)
+        var phaseText = 'Waxing Gibbous';
+    else if (checkDate > fullMoon && checkDate < lastQuarter)
+        var phaseText = 'Waning Gibbous';
+    else if (checkDate > lastQuarter)
+        var phaseText = 'Waning Crescent';
+
+    console.log(phaseText);
+    phaseInfo = lune.phase(checkDate);
 }
 
 // Log next new moon date to the console
