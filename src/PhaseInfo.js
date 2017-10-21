5605b4ead299a4a57e0d6203685127641802c108
Kate Haskell <fuzzcat@bactroid.net>
2017-10-21 19:33:28 -0400

    Updated material-ui and added Typography

--- src/PhaseInfo.js
+++ src/PhaseInfo.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import Typography from 'material-ui/Typography'
 import './PhaseInfo.css'
 
 const makeNextPhasesString = obj =>
   obj.nextNew > obj.nextFull
-    ? <div><div>Full Moon: {obj.nextFull.toString()}</div><div>New Moon: {obj.nextNew.toString()}</div></div>
-    : <div><div>New Moon: {obj.nextNew.toString()}</div><div>Full Moon: {obj.nextFull.toString()}</div></div>
+    ? <div><div><Typography type='subheading'>Full Moon:</Typography> {obj.nextFull.toString()}</div><div><Typography type='subheading'>New Moon:</Typography> {obj.nextNew.toString()}</div></div>
+    : <div><div><Typography type='subheading'>New Moon:</Typography> {obj.nextNew.toString()}</div><div><Typography type='subheading'>Full Moon:</Typography> {obj.nextFull.toString()}</div></div>
 
 const PhaseInfo = ({phaseObj}) => {
   return (
     <div id='phaseinfo'>
-      <div>{phaseObj.phaseText} ({phaseObj.illuminationPercent}%)</div>
+      <Typography type='title'>{phaseObj.phaseText} ({phaseObj.illuminationPercent}%)</Typography>
       {makeNextPhasesString(phaseObj)}
     </div>
   )
